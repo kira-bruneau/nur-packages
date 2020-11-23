f68007dbb4f895a231d61af10bbd6b9696356fc4
Kira Bruneau <kira.bruneau@gmail.com>
2020-11-22 23:09:44 -0500

    ccache: added support for darwin

--- pkgs/tools/misc/ccache/default.nix
+++ pkgs/tools/misc/ccache/default.nix
@@ -1,10 +1,14 @@
-{ stdenv
+{ lib
+, stdenv
 , fetchFromGitHub
 , fetchpatch
+, substituteAll
+, binutils
 , asciidoc
 , cmake
-, zstd
 , perl
+, zstd
+, xcodebuild
 , makeWrapper
 }:
 
@@ -19,7 +23,7 @@ let ccache = stdenv.mkDerivation rec {
     sha256 = "1frcplrv61m2iwc6jwycpbcz1101xl6s4sh8p87prdj98l60lyrx";
   };
 
-  # TODO: Remove patches in next release
+  # TODO: Remove patches from upstream in next release
   patches = [
     # Fix badly named man page filename
     (fetchpatch {
@@ -31,18 +35,29 @@ let ccache = stdenv.mkDerivation rec {
       url = "https://github.com/ccache/ccache/commit/294ff2face26448afa68e3ef7b68bf4898d6dc77.patch";
       sha256 = "0rx69qn41bgksc4m3p59nk5d6rz72rwnfska9mh5j62pzfm8axja";
     })
-  ];
+    # Fixes use of undeclared identifier 'CLONE_NOOWNERCOPY' on darwin
+    (fetchpatch {
+      url = "https://github.com/ccache/ccache/commit/411c010c3a5ee690cd42b23ffcf026ae009e2897.patch";
+      sha256 = "062s424d0d0psp6wjhmfnfn1s5dsrz403hdid5drm6l2san0jhq0";
+    })
+  ] ++ lib.optional stdenv.isDarwin (substituteAll {
+    src = ./force-objdump-on-darwin.patch;
+    objdump = "${binutils.bintools}/bin/objdump";
+  });
 
-  nativeBuildInputs = [ asciidoc cmake ];
+  nativeBuildInputs = [ asciidoc cmake perl ];
 
   buildInputs = [ zstd ];
 
   outputs = [ "out" "man" ];
 
   doCheck = true;
-  checkInputs = [ perl ];
-  preCheck = ''
+  checkInputs = lib.optional stdenv.isDarwin xcodebuild;
+  checkPhase = ''
     export HOME=$(mktemp -d)
+    ctest --output-on-failure ${lib.optionalString stdenv.isDarwin ''
+      -E '^(test.nocpp2|test.modules)$'
+    ''}
   '';
 
   passthru = {
@@ -92,7 +107,7 @@ let ccache = stdenv.mkDerivation rec {
     homepage = "https://ccache.dev";
     downloadPage = "https://ccache.dev/download.html";
     license = licenses.gpl3Plus;
-    maintainers = with maintainers; [ metadark ];
+    maintainers = with maintainers; [ metadark r-burns ];
     platforms = platforms.unix;
   };
 };
