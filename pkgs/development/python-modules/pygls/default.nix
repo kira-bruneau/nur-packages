311eb619a27b115341c1ae386ef3ce534f2e52c2
Kira Bruneau <kira.bruneau@pm.me>
2021-03-16 10:37:25 -0400

    pygls: 0.9.1 -> 0.10.0

--- pkgs/development/python-modules/pygls/default.nix
+++ pkgs/development/python-modules/pygls/default.nix
@@ -2,6 +2,9 @@
 , buildPythonPackage
 , isPy3k
 , fetchFromGitHub
+, psutil
+, pydantic
+, typeguard
 , mock
 , pytest-asyncio
 , pytestCheckHook
@@ -9,17 +12,27 @@
 
 buildPythonPackage rec {
   pname = "pygls";
-  version = "0.9.1";
+  version = "0.10.0";
   disabled = !isPy3k;
 
   src = fetchFromGitHub {
     owner = "openlawlibrary";
     repo = pname;
     rev = "v${version}";
-    hash = "sha256-uoHqHp56OoMtulnXyO3PwgBviqoSXw2UR0+ahlIp/ew=";
+    hash = "sha256-n+fO5b4CCau2yHwxGbsGQ4eCcmeI5pScME7HKfSFY/s=";
   };
 
-  checkInputs = [ mock pytest-asyncio pytestCheckHook ];
+  propagatedBuildInputs = [
+    psutil
+    pydantic
+    typeguard
+  ];
+
+  checkInputs = [
+    mock
+    pytest-asyncio
+    pytestCheckHook
+  ];
 
   meta = with lib; {
     description = "Pythonic generic implementation of the Language Server Protocol";
