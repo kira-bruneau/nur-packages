0b45a952cb04758077497d6dff1d21eec3b3d587
Kira Bruneau <kira.bruneau@pm.me>
2021-05-22 13:54:34 -0400

    nixos/bluetooth-autoconnect: prefer mkMerge over set concatenation

--- modules/services/networking/bluetooth-autoconnect.nix
+++ modules/services/networking/bluetooth-autoconnect.nix
@@ -37,31 +37,34 @@ in
         };
       };
 
-      user.services = mkIf config.hardware.pulseaudio.enable {
-        pulseaudio-bluetooth-autoconnect = {
-          description = "Bluetooth autoconnect service for pulseaudio";
-          wantedBy = [ "pulseaudio.service" ];
-          after = [ "pulseaudio.service" ];
-          serviceConfig = {
-            Type = "simple";
-            ExecStart = "${bluetooth-autoconnect}/bin/bluetooth-autoconnect";
+      user.services = mkMerge [
+        (mkIf config.hardware.pulseaudio.enable {
+          pulseaudio-bluetooth-autoconnect = {
+            description = "Bluetooth autoconnect service for pulseaudio";
+            wantedBy = [ "pulseaudio.service" ];
+            after = [ "pulseaudio.service" ];
+            serviceConfig = {
+              Type = "simple";
+              ExecStart = "${bluetooth-autoconnect}/bin/bluetooth-autoconnect";
+            };
           };
-        };
-      } // mkIf (config.services.pipewire.enable && config.services.pipewire.pulse.enable) {
-        pipewire-bluetooth-autoconnect = {
-          description = "Bluetooth autoconnect service for pipewire";
-          wantedBy = [ "pipewire.service" ];
-          after = [ "pipewire.service" "pipewire-pulse.service" ];
-          serviceConfig = {
-            Type = "simple";
+        })
+        (mkIf (config.services.pipewire.enable && config.services.pipewire.pulse.enable) {
+          pipewire-bluetooth-autoconnect = {
+            description = "Bluetooth autoconnect service for pipewire";
+            wantedBy = [ "pipewire.service" ];
+            after = [ "pipewire.service" "pipewire-pulse.service" ];
+            serviceConfig = {
+              Type = "simple";
 
-            # PipeWire is currently a simple service, so we can't know for sure when it will be ready
-            ExecStartPre = "${pkgs.coreutils}/bin/sleep 1";
+              # PipeWire is currently a simple service, so we can't know for sure when it will be ready
+              ExecStartPre = "${pkgs.coreutils}/bin/sleep 1";
 
-            ExecStart = "${bluetooth-autoconnect}/bin/bluetooth-autoconnect";
+              ExecStart = "${bluetooth-autoconnect}/bin/bluetooth-autoconnect";
+            };
           };
-        };
-      };
+        })
+      ];
     };
   };
 
