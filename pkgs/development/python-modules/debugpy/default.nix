8613df834c37f688b75c6174e469cb2ed5f0596f
Sandro Jäckel <sandro.jaeckel@gmail.com>
2021-07-20 23:08:56 +0200

    pythonPackages: deprecate pytest_xdist alias

--- pkgs/development/python-modules/debugpy/default.nix
+++ pkgs/development/python-modules/debugpy/default.nix
@@ -7,7 +7,7 @@
 , flask
 , psutil
 , pytest-timeout
-, pytest_xdist
+, pytest-xdist
 , pytestCheckHook
 , requests
 , isPy27
@@ -69,7 +69,7 @@ buildPythonPackage rec {
     flask
     psutil
     pytest-timeout
-    pytest_xdist
+    pytest-xdist
     pytestCheckHook
     requests
   ] ++ lib.optionals (!isPy27) [
