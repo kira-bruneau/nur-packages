e218cf79b1b722fd43e37baa75452702c0ae162a
Kira Bruneau <kira.bruneau@pm.me>
2025-02-27 00:01:25 -0500

    ender3-v3-se-klipper-with-display: 1.0.0-unstable-2025-02-26 -> 1.0.0-unstable-2025-02-27

--- pkgs/by-name/en/ender3-v3-se-klipper-with-display/package.nix
+++ pkgs/by-name/en/ender3-v3-se-klipper-with-display/package.nix
@@ -7,13 +7,13 @@
 
 klipper.overrideAttrs (finalAttrs: {
   pname = "ender3-v3-se-klipper-with-display";
-  version = "1.0.0-unstable-2025-02-26";
+  version = "1.0.0-unstable-2025-02-27";
 
   src = fetchFromGitHub {
     owner = "jpcurti";
     repo = "ender3-v3-se-klipper-with-display";
-    rev = "93886740c6f6f4bb4b233acc923c3e437f1bb428";
-    hash = "sha256-AcK8B7vju/Wojx3h7ERurPZLk0cpxG3JA0HDXc1tk3s=";
+    rev = "2e8b430806794255f33bf2e0db12f11e9ddd3ef3";
+    hash = "sha256-HBFSs//Ec9eJyZZf39XS5wgXP29qLZFvTCp6qDTN3xk=";
   };
 
   passthru.updateScript = nix-update-script { extraArgs = [ "--version=branch" ]; };
