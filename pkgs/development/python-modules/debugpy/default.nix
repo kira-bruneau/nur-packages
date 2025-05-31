NixOS/nixpkgs

python3Packages.debugpy: Enable riscv64 platform

pkgs/development/python-modules/debugpy/default.nix
@@ -84,6 +84,7 @@ buildPythonPackage rec {
             "x86_64-linux" = "-shared -o attach_linux_amd64.so";
             "i686-linux" = "-shared -o attach_linux_x86.so";
             "aarch64-linux" = "-shared -o attach_linux_arm64.so";
+            "riscv64-linux" = "-shared -o attach_linux_riscv64.so";
             "x86_64-darwin" = "-D_REENTRANT -dynamiclib -lc -o attach_x86_64.dylib";
             "aarch64-darwin" = "-D_REENTRANT -dynamiclib -lc -o attach_arm64.dylib";
           }
@@ -154,6 +155,7 @@ buildPythonPackage rec {
       "aarch64-linux"
       "x86_64-darwin"
       "aarch64-darwin"
+      "riscv64-darwin"
     ];
   };
 }

kira-bruneau/nur-packages

pkgs/development/python-modules/debugpy/default.nix
@@ -84,6 +84,7 @@ buildPythonPackage rec {
             "x86_64-linux" = "-shared -o attach_linux_amd64.so";
             "i686-linux" = "-shared -o attach_linux_x86.so";
             "aarch64-linux" = "-shared -o attach_linux_arm64.so";
+            "riscv64-linux" = "-shared -o attach_linux_riscv64.so";
             "x86_64-darwin" = "-D_REENTRANT -dynamiclib -lc -o attach_x86_64.dylib";
             "aarch64-darwin" = "-D_REENTRANT -dynamiclib -lc -o attach_arm64.dylib";
           }
@@ -154,6 +155,7 @@ buildPythonPackage rec {
       "aarch64-linux"
       "x86_64-darwin"
       "aarch64-darwin"
+      "riscv64-linux"
     ];
   };
 }