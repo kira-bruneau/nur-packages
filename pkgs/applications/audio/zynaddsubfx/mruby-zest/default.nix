a6a7b54758b251b4d5854cd32040babf0ef24afa
quantenzitrone <general@dev.quantenzitrone.eu>
2026-02-05 22:04:53 +0100

    various: rename references from libX11 to libx11
    this shouldn't create any rebuilds

--- pkgs/applications/audio/zynaddsubfx/mruby-zest/default.nix
+++ pkgs/applications/audio/zynaddsubfx/mruby-zest/default.nix
@@ -8,7 +8,7 @@
   ruby,
   libGL,
   libuv,
-  libX11,
+  libx11,
 }:
 
 stdenv.mkDerivation (finalAttrs: {
@@ -37,7 +37,7 @@ stdenv.mkDerivation (finalAttrs: {
   buildInputs = [
     libGL
     libuv
-    libX11
+    libx11
   ];
 
   # Force optimization to fix:
