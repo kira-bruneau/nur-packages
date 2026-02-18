e1635371a23fe43c1e78098ee432d29a1b49f9c6
Kira Bruneau <kira.bruneau@pm.me>
2026-02-17 19:37:44 -0500

    emacsPackages.majutsu: 0.6.0-unstable-2026-02-09 -> 0.6.0-unstable-2026-02-17

--- pkgs/applications/editors/emacs/elisp-packages/manual-packages/majutsu/package.nix
+++ pkgs/applications/editors/emacs/elisp-packages/manual-packages/majutsu/package.nix
@@ -9,13 +9,13 @@
 
 melpaBuild (finalAttrs: {
   pname = "majutsu";
-  version = "0.6.0-unstable-2026-02-09";
+  version = "0.6.0-unstable-2026-02-17";
 
   src = fetchFromGitHub {
     owner = "0WD0";
     repo = "majutsu";
-    rev = "58f047488097aa5ac929b7d32375ab9db16f827e";
-    hash = "sha256-Nv7k8y0ekaWSsFuMJm4+4pAD112HPNnU/xQDJI1qryo=";
+    rev = "fc431ac3c50cd8f6f7d6c24ac6302994f7b49f00";
+    hash = "sha256-DtO+fZFpNo9xTMvMx4FDQ253LgAncJN6YmQ0PQZzdG0=";
   };
 
   packageRequires = [
